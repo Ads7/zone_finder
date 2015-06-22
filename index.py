2282e23cf3767583540c22cf5a4d7bf4071ff483
Ads7 <ads71993@gmail.com>
2015-06-22 13:28:19 +0530

    1.2 workingscript post data not integrated

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 # unique abpi key registered to ads71993@gmail.com
 YOUR_API_KEY = 'AIzaSyBVWtEz2Ksqvde9hU1UmQur-Q44H3av9O0'
-google_places = GooglePlaces(YOUR_API_KEY)
+
 
 @app.route("/")
 def hello():
@@ -13,15 +13,16 @@ def hello():
 		name = request.args.get('name')
 	if request.args.get('location'):
 		location =  request.args.get('location')
+	google_places = GooglePlaces(YOUR_API_KEY)	
 	query_result = google_places.nearby_search(name='Kwality Restaurant',
 		location='karol bagh , delhi',keyword='chicken',
 		radius=20000, types=[types.TYPE_FOOD])
 	data = []
 	for place in query_result.places:
-		print data.append(place.name)
-		print data.append(place.geo_location)
-		print data.append(place.place_id)
-	return "hello"
+		print(place.name)
+		print(place.geo_location)
+		print(place.place_id)
+	return str(place.name)+str(place.geo_location)+str(place.place_id)
 
 if __name__ == "__main__":
     app.run()
